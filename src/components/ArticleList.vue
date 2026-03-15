459081d0d4a0cb76c0102543eefc8dc114bc89b8
lancegao <lancegao@lancegaodeMacBook-Pro.local>
2026-03-16 00:52:42 +0800

    上拉刷新

--- src/components/ArticleList.vue
+++ src/components/ArticleList.vue
@@ -17,7 +17,11 @@ const store = useChannelStore()
 const articles = ref<ArticleItem[]>([])
 const articleListRef = ref()
 const offset = 100 // 偏移量
+// 上拉加载文本
+const loadText = ref('')
 const distance = 100 // 偏移量
+// 下拉刷新提示文本
+const pullText = ref('')
 watch(()=>store.channelId, async ()=>{
   // 更新后执行的函数
   const res = await axios.get<ArticleResData>('http://geek.itheima.net/v1_0/articles', {
@@ -81,25 +85,69 @@ const debounce = (fn: any, delay = 200) => {
   }
 }
 
-const laodMoreData = (el: any) => {
+const loadMoreData = (el: any) => {
   let elClientH = el.target.clientHeight
   let elScrollTop = el.target.scrollTop
   let elScrollH = el.target.scrollHeight
-  console.log(elScrollH, elScrollTop, elClientH)
+    loadText.value = '加载更多...'
   if ((elScrollTop + elClientH) >= (elScrollH - distance)) {
     console.log('上拉加载数据')
+    loadText.value = '加载中...'
+    setTimeout(() => {
+     loadText.value = ''
+    })
   }
 }
 
 const handleThrottle = throttle(lazyLoadImgs, 200)
 
-const handleLoadMore = debounce(laodMoreData, 200)
+const handleLoadMore = debounce(loadMoreData, 200)
+
+// 下拉刷新
+const pullRefresh = () => {
+  // 初始位置
+  let initPosi = 0
+  // 下拉的最大距离
+  let maxPullDistance = 80
+  // 监听touchstart事件，记录初始位置，设置样式
+  articleListRef.value.addEventListener('touchstart', function(el: any) {
+    initPosi = el.touches[0].pageY
+    articleListRef.value.style.position = 'relative'
+    articleListRef.value.style.transition = `transform 0s`
+  })
+
+  // 监听touchmove事件，当下拉时将元素transform设置
+  articleListRef.value.addEventListener('touchmove', function(el: any) {
+    let curPosi = el.touches[0].pageY
+    let moveDistance = curPosi - initPosi
+    if (moveDistance > 0 && moveDistance < maxPullDistance) {
+      pullText.value = '下拉刷新...'
+      articleListRef.value.style.transform = `translateY(${moveDistance}px)`
+      if (moveDistance > 70) {
+        pullText.value = '释放刷新...'
+      }
+    }
+  })
+  // 监听touchend事件，将元素样式恢复
+  articleListRef.value.addEventListener('touchend', function(el: any) {
+    pullText.value = '更新中...'
+    articleListRef.value.style.transform = 'translateY(0px)'
+    articleListRef.value.style.translate = 'transform 0.5s ease 1s'
+    setTimeout(() => {
+      pullText.value = ''
+    }, 800)
+  })
+}
+
+
 
 onMounted(() => {
   // 添加监听事件
   articleListRef.value.addEventListener('scroll', handleThrottle)
   // 上拉加载数据 
   articleListRef.value.addEventListener('scroll', handleLoadMore)
+  // 下拉刷新数据
+  pullRefresh()
 })
 
 const lazyImgsObserver = new IntersectionObserver((entries, observer) => {
@@ -124,6 +172,7 @@ const intersectionObserverLazyImgs = () => {
 </script>
 
 <template>
+  <span class="pull-text" v-if="pullText">{{pullText}}</span>
   <div class="article-list" ref="articleListRef">
     <div class="article-item" v-for="item in articles" :key="item.art_id">
       <p class="title">{{ item.title }}</p>
@@ -141,4 +190,12 @@ const intersectionObserverLazyImgs = () => {
       </div>
     </div>
   </div>
+  <span class="load-text" v-if="loadText">{{loadText}}</span>
 </template>
+
+<style scoped>
+.pull-text {
+  text-align: center;
+  padding: 10px 0;
+}
+</style>
